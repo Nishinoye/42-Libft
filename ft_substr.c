4139c698a183922757462c5bbf49a68f941ea4c5
Teddy Carpin <tedcarpi@c1r3p3.42perpignan.fr>
2024-11-09 14:56:48 +0100

    autosave 2024-11-09 14:56:48

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: tedcarpi <tedcarpi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 09:30:04 by tedcarpi          #+#    #+#             */
-/*   Updated: 2024/11/09 14:50:43 by tedcarpi         ###   ########.fr       */
+/*   Updated: 2024/11/09 14:54:01 by tedcarpi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,12 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	unsigned int	i;
 
 	i = 0;
-	if (s[i])
+	if (s[i] == '\0')
 		return (NULL);
+	if (start >= ft_strlen(s))
+		len = 0;
 	if (len > ft_strlen(s) - start)
-		return (NULL);	
+		len = ft_strlen(s) - start;	
 	res = malloc(sizeof(char) * (len + 1));
 	if (res == NULL)
 		return (NULL);
