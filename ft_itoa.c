4a70489cb66dbe8c8917076b2f1dc836b2d0b533
Teddy Carpin <tedcarpi@c1r3p3.42perpignan.fr>
2024-11-09 16:43:03 +0100

    autosave 2024-11-09 16:43:03

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,15 +6,25 @@
 /*   By: tedcarpi <tedcarpi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/09 16:20:30 by tedcarpi          #+#    #+#             */
-/*   Updated: 2024/11/09 16:34:50 by tedcarpi         ###   ########.fr       */
+/*   Updated: 2024/11/09 16:43:01 by tedcarpi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static size_t	*lennbr(n)
+{
+	size_t	i;
+	
+	i = 0;
+	if (n < 10)
+		i = 1;
+	return (i)
+}
+
 char	*ft_itoa(int n)
 {
 	char	*res;
 	
-	res = (char *) malloc(sizeof(char) *) 
+	res = (char *) malloc(sizeof(char) * (lennbr(n) + 1)); 
 }
