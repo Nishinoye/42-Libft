61abdc4711372786acd8ac8cebb7912125334873
Teddy Carpin <tedcarpi@c1r3p3.42perpignan.fr>
2024-11-09 16:12:41 +0100

    autosave 2024-11-09 16:12:41

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: tedcarpi <tedcarpi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 15:28:56 by tedcarpi          #+#    #+#             */
-/*   Updated: 2024/11/09 16:04:08 by tedcarpi         ###   ########.fr       */
+/*   Updated: 2024/11/09 16:12:26 by tedcarpi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,9 @@ char	*ft_strnstr(const char *s1, const char *s2, size_t size)
 	while (s1[i] && i < size)
 	{
 		j = 0;
-		while (s1[i] == s2[j] && i < size && s2[j])
-		{
+		while (s1[i + j ] == s2[j] && 
+				i + j < size && s2[j] && s1[i + j])
 			j++;
-			i++;
-		}
 		if (s2[j] == '\0')
 			return ((char *)&s1[i]);
 		i++;
