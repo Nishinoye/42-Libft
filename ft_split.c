97421fb942a09fed5d94a9ea5124027d3df3d51f
Teddy Carpin <tedcarpi@c1r3p3.42perpignan.fr>
2024-11-09 14:26:27 +0100

    autosave 2024-11-09 14:26:27

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: tedcarpi <tedcarpi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/09 08:17:31 by tedcarpi          #+#    #+#             */
-/*   Updated: 2024/11/09 13:24:26 by tedcarpi         ###   ########.fr       */
+/*   Updated: 2024/11/09 14:25:06 by tedcarpi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,15 +48,16 @@ char	**ft_split(char const *s, char c)
 
 	i = 0;
 	x = 0;
-	res = (char **) malloc((sizeof(char *) * (countwords + 1)));
+	res = (char **) malloc((sizeof(char *) * (countwords(s, c) + 1)));
 	if (res == NULL)
 		return (NULL);
 	while (s[i] && i < ft_strlen(s))
 	{
 		while (s[i] == c)
 			i++;
-		res[x] = (char *) malloc(sizeof(char) * (lenword(s, c, ) + 1));
-		if (res)
+		res[x] = (char *) malloc(sizeof(char) * (lenword(s, c, i) + 1));
+		if (res == NULL)
+		{}
 		res[x] = ft_substr(s, i, lenword);
 		x++;
 		while (s[i] != c && i < ft_strlen(s))
