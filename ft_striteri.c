ef4f9105eaf14b9fe0dc2145a1fa1290359a180e
Teddy Carpin <tedcarpi@c2r3p3.42perpignan.fr>
2024-11-10 15:17:00 +0100

    autosave 2024-11-10 15:17:00

--- ft_striteri.c
+++ ft_striteri.c
@@ -1,32 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tedcarpi <tedcarpi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/10 14:52:03 by tedcarpi          #+#    #+#             */
-/*   Updated: 2024/11/10 15:14:59 by tedcarpi         ###   ########.fr       */
+/*   Created: 2024/11/10 15:16:21 by tedcarpi          #+#    #+#             */
+/*   Updated: 2024/11/10 15:16:22 by tedcarpi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//
-
-#include "libft.h"
-
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
-{
-	char			*res;
-	unsigned int	i;
-
-	res = ((char *) ft_calloc(ft_strlen(s) + 1, sizeof(char)));
-	if (!res)
-		return (NULL);
-	i = 0;
-	while (s[i])
-	{
-		res[i] = f(i, s[i]);
-		i++;
-	}
-	return (res);
-}
