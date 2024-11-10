9960f4f56c6678a5a051010f4053b2d36269fd90
Teddy Carpin <tedcarpi@c2r3p3.42perpignan.fr>
2024-11-10 15:06:56 +0100

    autosave 2024-11-10 15:06:56

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,13 +6,20 @@
 /*   By: tedcarpi <tedcarpi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/10 14:52:03 by tedcarpi          #+#    #+#             */
-/*   Updated: 2024/11/10 14:53:52 by tedcarpi         ###   ########.fr       */
+/*   Updated: 2024/11/10 15:06:55 by tedcarpi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 //
 
+#include "libft.h"
+
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
+	char			*res;
+	unsigned int	i;
 	
+	res = ((char *) ft_calloc(s, sizeof()))
+	i = 0;
+	f(s[i], i)
 }
