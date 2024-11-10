7104a09f1096cceb212bcc579d5266373dde55ca
Teddy Carpin <tedcarpi@c2r3p3.42perpignan.fr>
2024-11-10 11:10:22 +0100

    autosave 2024-11-10 11:10:22

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: tedcarpi <tedcarpi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 15:35:28 by tedcarpi          #+#    #+#             */
-/*   Updated: 2024/11/10 10:59:41 by tedcarpi         ###   ########.fr       */
+/*   Updated: 2024/11/10 11:00:58 by tedcarpi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t size)
 	i = 0;
 	ptr1 = (unsigned char *)src;
 	ptr2 = (unsigned char *)dest;
-	while (i < size && ptr2[i])
+	while (i < size)
 	{
 		ptr2[i] = ptr1[i];
 		i++;
--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: tedcarpi <tedcarpi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 10:39:59 by tedcarpi          #+#    #+#             */
-/*   Updated: 2024/11/10 10:59:46 by tedcarpi         ###   ########.fr       */
+/*   Updated: 2024/11/10 11:09:03 by tedcarpi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,11 @@
 
 size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 {
-	size_t	i;
-
-	i = ft_strlen(src);
+	
 	if (size > 0)
 	{
-		ft_memcpy(dest, src, i);
-		dest[i] = '\0';
+		ft_memcpy(dest, src, size - 1);
+		dest[ft_strlen(src)] = '\0';
 	}
 	return (i);
 }
