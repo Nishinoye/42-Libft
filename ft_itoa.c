54e48ec773269b97600de947e3931d2fcca15eb1
Teddy Carpin <tedcarpi@c2r3p3.42perpignan.fr>
2024-11-10 14:16:39 +0100

    autosave 2024-11-10 14:16:39

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: tedcarpi <tedcarpi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/10 09:11:13 by tedcarpi          #+#    #+#             */
-/*   Updated: 2024/11/10 14:06:32 by tedcarpi         ###   ########.fr       */
+/*   Updated: 2024/11/10 14:13:02 by tedcarpi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,6 @@ static int	nblen(int n)
 	i = 0;
 	if (n == 0)
 		return (1);
-	if (n == INT_MIN)
-		return (11);
 	if (n < 0)
 	{
 		i++;
@@ -38,17 +36,19 @@ char	*ft_itoa(int n)
 {
 	char	*res;
 	int		len;
-	
+
 	if (n == INT_MIN)
+		return (ft_strdup("-2147483648"));
 	len = nblen(n);
 	res = (char *) ft_calloc(len + 1, sizeof(char));
 	if (!res)
 		return (NULL);
 	if (n < 0)
-	{	n *= -1;
+	{	
+		n *= -1;
 		res[0] = 45;
 	}
-	while (n >  9)
+	while (n > 9)
 	{
 		len --;
 		res[len] = n % 10 + 48;
@@ -56,24 +56,4 @@ char	*ft_itoa(int n)
 	}
 	res[len - 1] = n + 48;
 	return (res);
-}
-
-int	main()
-{
-	char	*res;
-	
-	res = ft_itoa(0);
-	printf("%s\n", res);
-	res = ft_itoa(5);
-	printf("%s\n", res);
-	res = ft_itoa(-5);
-	printf("%s\n", res);
-	res = ft_itoa(55555);
-	printf("%s\n", res);
-	res = ft_itoa(-55555);
-	printf("%s\n", res);
-	res = ft_itoa(INT_MAX);
-	printf("%s\n", res);
-	res = ft_itoa(INT_MIN);
-	printf("%s\n", res);
 }
