1b7dd12e08e14edf7b79058591515e055984b21e
Teddy Carpin <tedcarpi@c2r4p1.42perpignan.fr>
2024-11-13 16:55:34 +0100

    autosave 2024-11-13 16:55:34

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: tedcarpi <tedcarpi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 15:22:26 by tedcarpi          #+#    #+#             */
-/*   Updated: 2024/11/13 16:44:47 by tedcarpi         ###   ########.fr       */
+/*   Updated: 2024/11/13 16:52:14 by tedcarpi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,14 @@
 
 #include "libft.h"
 
-void ft_lstadd_back(t_list **lst, t_list *new)
+void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-	t_list	*i;
-	
+	t_list	*ptr;
+
 	if (!lst || !new)
 		return ;
-	i = ft_lstlast((*lst));
+	ptr = ft_lstlast((*lst));
+	(*lst) = ptr;
 	(*lst)->next = new;
 	new->next = NULL;
 }
