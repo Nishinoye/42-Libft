cf00c60f4dd95805bf3840a889591f7672506d0d
Teddy Carpin <tedcarpi@c2r4p1.42perpignan.fr>
2024-11-13 17:25:46 +0100

    autosave 2024-11-13 17:25:46

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: tedcarpi <tedcarpi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 08:54:48 by tedcarpi          #+#    #+#             */
-/*   Updated: 2024/11/13 17:13:02 by tedcarpi         ###   ########.fr       */
+/*   Updated: 2024/11/13 17:24:52 by tedcarpi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 #include "libft.h"
 
-t_list	*ft_lstnew(void *content)
+t_list	*ft_lstnew(int ex)
 {
 	t_list	*new;
 
 	new = malloc(sizeof(t_list));
 	if (!new)
 		return (NULL);
-	new->content = content;
+	new->content = ex;
 	new->next = NULL;
 	return (new);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: tedcarpi <tedcarpi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 13:24:19 by tedcarpi          #+#    #+#             */
-/*   Updated: 2024/11/13 15:23:24 by tedcarpi         ###   ########.fr       */
+/*   Updated: 2024/11/13 17:23:59 by tedcarpi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
 
 typedef struct s_list
 {
-	void			*content;
+	int			content;
 	struct s_list	*next;
 }			t_list;
 
