d9ae1c113848542fbfe050844b4ca417887ffe4f
Teddy Carpin <tedcarpi@c2r4p1.42perpignan.fr>
2024-11-13 11:43:40 +0100

    autosave 2024-11-13 11:43:40

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -6,7 +6,7 @@
 /*   By: tedcarpi <tedcarpi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 09:26:47 by tedcarpi          #+#    #+#             */
-/*   Updated: 2024/11/13 11:20:15 by tedcarpi         ###   ########.fr       */
+/*   Updated: 2024/11/13 11:38:01 by tedcarpi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,6 @@ void	ft_lstadd_front(t_list **lst, t_list *new)
 {
 	if (!lst || !new)
 		exit(EXIT_FAILURE);
-	new->next = *lst;
-	*lst = new;
+	new->next = lst[0];
+	lst[0] = new;
 }
