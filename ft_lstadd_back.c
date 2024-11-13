4a74298081e41dd51940201e0e4ca2fd78592d8d
Teddy Carpin <tedcarpi@c2r4p1.42perpignan.fr>
2024-11-13 16:05:16 +0100

    autosave 2024-11-13 16:05:16

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: tedcarpi <tedcarpi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 15:22:26 by tedcarpi          #+#    #+#             */
-/*   Updated: 2024/11/13 15:50:19 by tedcarpi         ###   ########.fr       */
+/*   Updated: 2024/11/13 15:58:11 by tedcarpi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,8 @@ void ft_lstadd_back(t_list **lst, t_list *new)
 {
 	if (!lst || !new)
 		return ;
-	while (*lst->next != NULL)
-		lst = *lst->next;
+	while (lst->next != NULL)
+		lst = lst->next;
+	
 	
 }
