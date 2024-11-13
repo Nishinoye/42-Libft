33f22d8a1f194e5725e086c3b41a03634d3892c6
Teddy Carpin <tedcarpi@c2r4p1.42perpignan.fr>
2024-11-13 09:22:48 +0100

    autosave 2024-11-13 09:22:48

--- ft_lstnez.c
+++ ft_lstnez.c
@@ -6,7 +6,7 @@
 /*   By: tedcarpi <tedcarpi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 08:54:48 by tedcarpi          #+#    #+#             */
-/*   Updated: 2024/11/13 09:12:09 by tedcarpi         ###   ########.fr       */
+/*   Updated: 2024/11/13 09:15:37 by tedcarpi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,12 @@
 
 t_list *ft_lstnew(void *content)
 {
+	t_list	*new;
 	
+	new = malloc(sizeof(t_list));
+	if (!new)
+		return(NULL);
+	new->content = content;
+	new->next = NULL;
+	return (new);
 }
