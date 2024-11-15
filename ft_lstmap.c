76bb4ee859e74591f2fc8a5d3a23170366ef5885
Teddy Carpin <tedcarpi@c2r4p8.42perpignan.fr>
2024-11-15 16:34:53 +0100

    autosave 2024-11-15 16:34:53

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: tedcarpi <tedcarpi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 17:48:57 by tedcarpi          #+#    #+#             */
-/*   Updated: 2024/11/14 18:12:01 by tedcarpi         ###   ########.fr       */
+/*   Updated: 2024/11/15 16:34:01 by tedcarpi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,28 @@
 
 #include "libft.h"
 
-t_list *ft_lstmap(t_list *lst, void *(*f)(void *),void (*del)(void *))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new;
+	t_list	*swap;
+	t_list	*res;
 
-	while (lst)
+	if (!lst)
+		return (NULL);
+	swap = lst;
+	res = NULL;
+	while (swap)
 	{
-		new = f(lst->content);
-		lst = lst->next;
-		
+		new = ft_lstnew(f(swap->content));
+		if (!new)
+		{
+			ft_lstclear(&new, del);
+			ft_lstclear(&res, del);
+			ft_lstclear(&swap, del);		
+		}
+		ft_lstadd_back(&res, new);
+		swap = swap->next;
 	}
-	
+	ft_lstclear(&swap, del);
+	return (res);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: tedcarpi <tedcarpi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 13:24:19 by tedcarpi          #+#    #+#             */
-/*   Updated: 2024/11/14 17:49:35 by tedcarpi         ###   ########.fr       */
+/*   Updated: 2024/11/15 15:10:22 by tedcarpi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,7 @@ void	ft_lstclear(t_list **lst, void (*del)(void*));
 void	ft_lstdelone(t_list *lst, void (*del)(void*));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
 t_list	*ft_lstlast(t_list *lst);
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *),void (*del)(void *));
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 t_list	*ft_lstnew(void *content);
 int		ft_lstsize(t_list *lst);
 void	*ft_memchr(const void *block, int src, size_t size);
